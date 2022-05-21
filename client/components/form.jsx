212568dccc05cbe84667c55f1618eb01af00320c
Shuwen Liang <fidwrs@gmail.com>
2022-05-20 17:31:56 -0700

    create a form and a submit button

--- client/components/form.jsx
+++ client/components/form.jsx
@@ -4,7 +4,13 @@ import { Context } from '../context/context.js';
 const Form = () => {
   return(
     <div>
-      <h3>Enter prompt</h3>
+      <p>Enter prompt</p>
+      <div className="enter">
+        <textarea rows="10" cols="40"></textarea>
+      </div>
+      <div className="button-section">
+        <button type="submit">Submit</button>
+      </div>
     </div>
   )
 }
